e975039e983b5c925ef179170a349902bf298cf3
Minsub Song <minsub1031@gmail.com>
2023-09-08 16:30:48 +0900

    shibal

--- test.py
+++
@@ -1,7 +0,0 @@
-from roboflow import Roboflow
-rf = Roboflow(api_key="onmlowwu2Z6sNNupWM5Z")
-project = rf.workspace().project("webtoon-bubble-detection")
-model = project.version(8).model
-
-# infer on a local image
-print(type(model.predict("test2.PNG", confidence=40, overlap=30).json()))
